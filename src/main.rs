fdc744b98fb170a71097f512ef0f1de63c6c1537
Eyup Gurel <eyupgurel@gmail.com>
2024-01-22 13:23:39 +0300

    ditched Diesel and SQLLite

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,13 @@ mod sockets;
 mod models;
 mod env;
 
-extern crate diesel;
 extern crate dotenv;
 
 
-use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
-use diesel::select;
-
-
-use std::{fmt, fs, iter, thread};
+use std::{iter};
 use std::collections::{BTreeMap, HashMap};
-use std::sync::mpsc;
-use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
+
+use std::time::{Duration, Instant};
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use chrono::{Utc};
@@ -28,114 +22,6 @@ use rdkafka::consumer::{Consumer, StreamConsumer};
 use rdkafka::Message;
 use serde_derive::{Deserialize, Serialize};
 
-table! {
-    orders (id) {
-        id -> BigInt,
-        market -> Text,
-        price -> BigInt,
-        timestamp -> BigInt,
-        quantity -> BigInt,
-        trigger_price -> BigInt,
-        leverage -> BigInt,
-        expiration -> BigInt,
-        salt -> BigInt,
-        maker -> Text,
-        flags -> Text,
-    }
-}
-
-const CREATE_ORDERS_TABLE: &str = "
-CREATE TABLE orders (
-    id INTEGER PRIMARY KEY,
-    market TEXT NOT NULL,
-    price INTEGER NOT NULL,
-    timestamp INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    trigger_price INTEGER NOT NULL,
-    leverage INTEGER NOT NULL,
-    expiration INTEGER NOT NULL,
-    salt INTEGER NOT NULL,
-    maker TEXT NOT NULL,
-    flags TEXT NOT NULL
-);
-
-CREATE INDEX idx_orders_market ON orders(market);
-CREATE INDEX idx_orders_price ON orders(price);
-CREATE INDEX idx_orders_timestamp ON orders(timestamp);
-";
-
-
-pub fn establish_connection() -> SqliteConnection {
-    let mut connection = SqliteConnection::establish(":memory:")
-        .expect("Error connecting to in-memory SQLite");
-
-    diesel::sql_query(CREATE_ORDERS_TABLE)
-        .execute(&mut connection)
-        .expect("Error creating posts table");
-
-    connection
-}
-
-#[derive(Insertable)]
-#[table_name = "orders"]
-struct NewOrder<'a> {
-    id: i64,
-    market: &'a str,
-    price: i64,
-    timestamp: i64,
-    quantity: i64,
-    trigger_price: i64,
-    leverage: i64,
-    expiration: i64,
-    salt: i64,
-    maker: &'a str,
-    flags: &'a str,
-}
-
-// Define a struct to represent the fields you want to update
-#[derive(AsChangeset)]
-#[table_name = "orders"]
-struct OrderUpdate<'a> {
-    price: Option<i64>,
-    quantity: Option<i64>,
-    trigger_price: Option<i64>,
-    leverage: Option<i64>,
-    expiration: Option<i64>,
-    maker: Option<&'a str>,
-    flags: Option<&'a str>,
-}
-
-// Function to update an order
-fn update_order<'a>(
-    connection: &mut SqliteConnection,
-    order_id: i64,
-    new_price: Option<i64>,
-    new_quantity: Option<i64>,
-    new_trigger_price: Option<i64>,
-    new_leverage: Option<i64>,
-    new_expiration: Option<i64>,
-    new_maker: Option<&'a str>,
-    new_flags: Option<&'a str>,
-) -> QueryResult<usize> {
-    use self::orders::dsl::*;
-
-    // Create an instance of OrderUpdate with the new values
-    let updated_order = OrderUpdate {
-        price: new_price,
-        quantity: new_quantity,
-        trigger_price: new_trigger_price,
-        leverage: new_leverage,
-        expiration: new_expiration,
-        maker: new_maker,
-        flags: new_flags,
-    };
-
-    // Update the order with the specified ID
-    diesel::update(orders.filter(id.eq(order_id)))
-        .set(&updated_order)
-        .execute(connection)
-}
-
 
 fn generate_random_string(len: usize) -> String {
     let mut rng = rand::thread_rng();
@@ -144,17 +30,6 @@ fn generate_random_string(len: usize) -> String {
         .map(char::from)
         .take(len)
         .collect()
-}
-
-fn get_orders_ordered_by_price_timestamp(connection: &mut SqliteConnection, perp: &str) -> QueryResult<Vec<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>> {
-    use self::orders::dsl::*;
-
-    let result = orders
-        .filter(market.eq(perp)) // Filter orders by the specified market
-        .order((price.asc(), timestamp.asc())) // Orders by price ascending, then by timestamp ascending
-        .load::<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>(connection);
-
-    result
 }
 
 fn get_book_orders_ordered_by_price_timestamp<'a>(
@@ -172,37 +47,6 @@ fn get_book_orders_ordered_by_price_timestamp<'a>(
     orders
 }
 
-fn get_snapshot_data(connection: &mut SqliteConnection) -> QueryResult<Vec<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>> {
-    use self::orders::dsl::*;
-    
-    let result = orders
-        // .order((price.asc(), timestamp.asc()))// Orders by price ascending, then by timestamp ascending
-        .load::<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>(connection);
-    
-    result
-}
-
-fn generate_random_order<'a>(id:i64, markets: &'a [&'a str], makers: &'a Vec<String>, flags: &'a Vec<String>) -> NewOrder<'a> {
-    let mut rng = rand::thread_rng();
-    let market = markets[rng.gen_range(0..markets.len())]; // Randomly choose a market
-    let maker = &makers[rng.gen_range(0..makers.len())];
-    let flag = &flags[rng.gen_range(0..flags.len())]; // Select a random flag from the Vec<String>
-    let timestamp = Utc::now().timestamp(); // Generate a real timestamp
-
-    NewOrder {
-        id,
-        market,
-        price: rng.gen_range(50_i64..=250),
-        timestamp: timestamp + rng.gen_range(10..=250),
-        quantity: rng.gen_range(10_i64..=25),
-        trigger_price: rng.gen::<u64>() as i64,
-        leverage: rng.gen_range(1..=20),
-        expiration: rng.gen::<u64>() as i64,
-        salt: rng.gen::<u64>() as i64,
-        maker,
-        flags: flag, // Assign the randomly selected flag as a &str
-    }
-}
 
 fn generate_random_makers_and_flags(count: usize) -> (Vec<String>, Vec<String>) {
     let mut makers = Vec::new();
@@ -216,65 +60,6 @@ fn generate_random_makers_and_flags(count: usize) -> (Vec<String>, Vec<String>) 
     (makers, flags)
 }
 
-fn generate_random_orders<'a>(markets: &'a [&'a str], makers: &'a Vec<String>, flags: &'a Vec<String>, count: usize) -> Vec<NewOrder<'a>> {
-    let mut orders = Vec::new();
-    for i in 0..count {
-        let new_order = generate_random_order(i as i64, markets, makers, flags);
-        orders.push(new_order);
-    }
-    orders
-}
-
-fn delete_all_orders(connection: &mut SqliteConnection) -> QueryResult<usize> {
-    use self::orders::dsl::*;
-
-    diesel::delete(orders).execute(connection)
-}
-
-fn delete_orders_by_ids(connection: &mut SqliteConnection, order_ids: Vec<i64>) -> QueryResult<usize> {
-    use self::orders::dsl::*;
-
-    //let old_count = orders.count().first::<i64>(connection);
-
-    let deleted_rows = diesel::delete(orders.filter(id.eq_any(order_ids)))
-        .execute(connection)?;
-
-    //let new_count = orders.count().first::<i64>(connection);
-
-    Ok(deleted_rows)
-}
-
-fn delete_order_by_id(connection: &mut SqliteConnection, order_id: i64) -> QueryResult<usize> {
-    use self::orders::dsl::*;
-
-    let deleted_rows = diesel::delete(orders.filter(id.eq(order_id)))
-        .execute(connection)?;
-
-    Ok(deleted_rows)
-}
-
-fn convert_order_book_entries_to_new_orders<'a>(
-    orders_data: &'a Vec<(i64, i64)>,
-    market: &'a str,
-    maker: &'a str,
-    flags: &'a str
-) -> Vec<NewOrder<'a>> {
-    orders_data.iter().map(|&(price, quantity)| {
-        NewOrder {
-            id:price,
-            market,
-            price,
-            timestamp: Utc::now().timestamp(),
-            quantity,
-            trigger_price: 0,
-            leverage: 1,
-            expiration: 0,
-            salt: rand::random(),
-            maker,
-            flags,
-        }
-    }).collect()
-}
 
 #[derive(Debug)]
 pub struct Match {
@@ -296,7 +81,7 @@ impl Match {
 fn match_and_process_orders(
     order_book: &mut BTreeMap<u128, BTreeMap<i64, HashMap<String, BookOrder>>>,
     mut quantity: u128
-) -> QueryResult<Match> {
+) -> Option<Match> {
     let mut orders_to_process = Vec::new();
     let mut total_matched_quantity = 0;
     let mut matched = Match::new();
@@ -338,13 +123,11 @@ fn match_and_process_orders(
         }
     }
 
-    Ok(matched)
+    Some(matched)
 }
 
 
-fn generate_and_process_random_order( order_book: &mut BTreeMap<u128, BTreeMap<i64, HashMap<String, BookOrder>>>,
-
-) -> QueryResult<Match> {
+fn generate_and_process_random_order( order_book: &mut BTreeMap<u128, BTreeMap<i64, HashMap<String, BookOrder>>>) -> Option<Match> {
     // Generate a random order
     let random_order = generate_rand_order(); // '0' for id, it will be auto-generated by the database
 
@@ -595,83 +378,9 @@ async fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use diesel::connection::SimpleConnection;
     use std::{time::{Instant, Duration}, hash::Hash};
-    use diesel::dsl::Order;
-    use diesel::sql_query;
     use rand::prelude::{IteratorRandom, SliceRandom};
 
-    // Helper function to create and populate the test database
-    fn setup_test_database(count: usize) -> SqliteConnection {
-        let mut connection = establish_connection();
-
-        let markets = ["ETH-PERP", "BTC-PERP", "SOL-PERP"];
-        let (makers, flags) = generate_random_makers_and_flags(count);
-
-        let random_orders = generate_random_orders(&markets, &makers, &flags, count);
-
-        for new_order in &random_orders {
-            diesel::insert_into(orders::table)
-                .values(new_order)
-                .execute(&mut connection)
-                .expect("Error inserting new order");
-        }
-
-        connection
-    }
-
-    #[test]
-    fn test_get_orders_ordered_by_price_timestamp() {
-        // Arrange: Setup the test database with orders
-        let mut connection = setup_test_database(100000);
-
-        // Act: Get the orders and check if they are ordered correctly
-        let orders = get_orders_ordered_by_price_timestamp(&mut connection, "ETH-PERP")
-            .expect("Error fetching orders");
-
-        // Assert: Check if the orders are ordered by price and then by timestamp
-        let mut prev_price = i64::MIN;
-        let mut prev_timestamp = i64::MIN;
-
-        for (_, _, price, timestamp, _, _, _, _, _, _, _) in orders {
-            assert!(price >= prev_price);
-            if price == prev_price {
-                assert!(timestamp >= prev_timestamp);
-            }
-            prev_price = price;
-            prev_timestamp = timestamp;
-        }
-    }
-
-    #[test]
-    fn test_insert_orders_duration() {
-        // Arrange: Setup the test database
-        let mut connection = establish_connection();
-        let markets = ["ETH-PERP", "BTC-PERP", "SOL-PERP"];
-        let order_size: usize = 100000;
-        let (makers, flags) = generate_random_makers_and_flags(order_size);
-        let random_orders = generate_random_orders(&markets, &makers, &flags, order_size);
-
-        // Act: Measure the time it takes to insert orders
-        let start_time = Instant::now();
-        for new_order in &random_orders {
-            diesel::insert_into(orders::table)
-                .values(new_order)
-                .execute(&mut connection)
-                .expect("Error inserting new order");
-        }
-        let end_time = Instant::now();
-        let duration = end_time - start_time;
-        println!("insert orders duration: {:?}", duration);
-
-        // Assert: Check if the insertion duration is within an expected range
-        let max_expected_duration = Duration::from_secs(3); // Adjust this as needed
-        assert!(
-            duration <= max_expected_duration,
-            "Insertion took longer than expected: {:?}",
-            duration
-        );
-    }
 
     #[test]
     fn test_native_insert_orders_duration() {
@@ -966,160 +675,14 @@ mod tests {
         }
 
     }
-    #[test]
-    fn test_get_orders_ordered_by_price_timestamp_duration() {
-        // Arrange: Setup the test database
-        let mut connection = setup_test_database(500);
-
-        // Act: Measure the time it takes to insert orders
-        let start_time = Instant::now();
-
-        // Act: Get the orders and check if they are ordered correctly
-        let orders = get_orders_ordered_by_price_timestamp(&mut connection, "ETH-PERP")
-            .expect("Error fetching orders");
-        let end_time = Instant::now();
-        let duration = end_time - start_time;
-        println!("get orders duration: {:?}", duration);
 
 
-        // Assert: Check if the insertion duration is within an expected range
-        let max_expected_duration = Duration::from_millis(1); // Adjust this as needed
-        assert!(
-            duration <= max_expected_duration,
-            "Getting orders ordered by price and timestamp took longer than expected: {:?}",
-            duration
-        );
-    }
-
-    #[test]
-    fn test_delete_random_orders() {
-        // Arrange: Setup the test database with orders
-        let mut connection = setup_test_database(100000);
-        let market = "ETH-PERP";
-
-        // Get all order IDs in the database
-        let order_ids: Vec<i64> = orders::table
-            .select(orders::id)
-            .load(&mut connection)
-            .expect("Error loading order IDs");
-
-
-        // Randomly select a subset of order IDs to delete
-        let mut rng = rand::thread_rng();
-
-        let random_order_ids: Vec<i64> = order_ids
-            .choose_multiple(&mut rng, 50000) // Choose 10 random IDs
-            .cloned()
-            .collect();
-
-        let start_time = Instant::now();
-
-        let res = delete_orders_by_ids(&mut connection, random_order_ids).expect("Error deleting order");
-
-        let end_time = Instant::now();
-        let duration = end_time - start_time;
-        println!("delete orders duration: {:?}", duration);
-
-        let max_expected_duration = Duration::from_millis(100); // Adjust this as needed
-        assert!(
-            duration <= max_expected_duration,
-            "Deleting orders took longer than expected: {:?}",
-            duration
-        );
-    }
-
-
-    #[test]
-    fn test_fetch_an_order() {
-        // Arrange: Setup the test database with orders
-        let mut connection = setup_test_database(100000);
-
-        let start_time = Instant::now();
-        // Get all order IDs in the database
-        let order = orders::table.find(500).load::<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>(&mut connection);
-
-        let end_time = Instant::now();
-        let duration = end_time - start_time;
-        println!("Order: {:?}", order);
-        println!("Order fetch duration: {:?}", duration);
-
-        let max_expected_duration = Duration::from_millis(2); // Adjust this as needed
-        assert!(
-            duration <= max_expected_duration,
-            "Fetching an order took longer than expected: {:?}",
-            duration
-        );
-    }
-
-    #[test]
-    fn test_update_order() {
-        // Arrange: Setup the test database with orders
-        let mut connection = setup_test_database(100000); // Adjust the number as needed
-
-        let start_time = Instant::now();
-        // Act: Update the inserted order
-        let updated_rows = update_order(
-            &mut connection,
-            1, // Use the ID of the inserted order
-            Some(200), // New price
-            Some(25), // New quantity
-            None, // Not updating trigger_price
-            None, // Not updating leverage
-            None, // Not updating expiration
-            Some("UpdatedMaker"), // New maker
-            None,  // Not updating flags
-        ).expect("Error updating order");
-
-        let duration = Instant::now() - start_time;
-        println!("Updating an order: {:?}",duration);
-
-        // Assert: Verify that the order was updated correctly
-        assert_eq!(updated_rows, 1);
-
-        // Fetch the updated order and verify the changes
-        let updated_order = orders::table.find(1)
-            .first::<(i64, String, i64, i64, i64, i64, i64, i64, i64, String, String)>(&mut connection)
-            .expect("Error fetching updated order");
-
-        assert_eq!(updated_order.2, 200); // Check if price is updated to 200
-        assert_eq!(updated_order.4, 25); // Check if quantity is updated to 25
-        assert_eq!(updated_order.9, "UpdatedMaker"); // Check if maker is updated*/
-    }
-
-
-    #[test]
-    fn test_delete_all_orders() {
-        let mut connection = setup_test_database(100000);
-
-        let start_time = Instant::now();
-        // Perform the delete operation
-        let delete_count = delete_all_orders(&mut connection).expect("Error deleting orders");
-        
-        let duration = Instant::now() - start_time;
-        // Assert that 10 records were deleted
-        assert_eq!(delete_count, 100000);
-        
-        println!("duration to delete all orders: {:?}",duration);
-
-        let max_expected_duration = Duration::from_millis(3); // Adjust this as needed
-
-        assert!(
-            duration <= max_expected_duration,
-            "Deleting all orders took longer than expected: {:?}",
-            duration
-        );
-
-        let orders = get_orders_ordered_by_price_timestamp(&mut connection, "ETH-PERP")
-            .expect("Error fetching orders");
-
-        assert_eq!(orders.len(),0);
-    }
-
-    #[test]
+/*    #[test]
     fn test_snapshot() {
         use std::fs::File;
         use std::io::Write;
-        let mut connection = setup_test_database(1000);
+
+        let random_orders = generate_rand_orders(1000);
 
         let start_time = Instant::now();
         let mut start = Instant::now();
@@ -1162,7 +725,7 @@ mod tests {
             "Taking snapshot took longer than expected: {:?}",
             duration
         );
-    }
+    }*/
 
     #[test]
     fn test_upsert_order() {
